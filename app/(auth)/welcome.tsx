d861e185edd53db39bbe4fed3e81d7162d537e97
emammad22 <mammadoveltun22@gmail.com>
2024-12-19 18:46:14 +0400

    app setup & onboarding screen

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,50 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { useRef, useState } from "react";
+import { onboarding } from "../../constants/index";
+import CustomButton from "@/components/CustomButton";
+
+const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding?.length - 1;
+
+  return (
+    <SafeAreaView className="flex h-full justify-center items-center">
+      <TouchableOpacity
+        onPress={() => router.replace("/(auth)/sign-up")}
+        className="w-full flex justify-end items-end p-5">
+        <Text className="text-md text-black font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />}
+        activeDot={<View className="w-[32px] h-[4px] mx-1 bg-[#0286ff] rounded-full" />}
+        onIndexChanged={(index) => setActiveIndex(index)}>
+        {onboarding?.map((item, index) => {
+          return (
+            <View key={item.id} className="flex justify-center items-center p-5">
+              <Image source={item.image} className="w-full h-[300px]" resizeMode="contain" />
+              <View className="flex justify-center items-center w-full mt-10 flex-row">
+                <Text className="font-bold text-black text-3xl mx-10 text-center">{item.title}</Text>
+              </View>
+              <Text className="text-lg font-JakartaSemiBold text-center test-[#858585] mx-10 mt-3">
+                {item.description}
+              </Text>
+            </View>
+          );
+        })}
+      </Swiper>
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() => (isLastSlide ? router.replace("/(auth)/sign-up") : swiperRef?.current?.scrollBy(1))}
+        className={"w-11/12 mt-10"}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
